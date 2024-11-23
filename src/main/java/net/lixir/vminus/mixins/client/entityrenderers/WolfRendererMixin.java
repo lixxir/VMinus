013343d1050826f40a974beb1678dc37a09974fd
lixxir <muffinsqw@outlook.com>
2024-11-23 17:28:01 -0400

    Fixed syntax error

--- src/main/java/net/lixir/vminus/mixins/client/entityrenderers/WolfRendererMixin.java
+++ src/main/java/net/lixir/vminus/mixins/client/entityrenderers/WolfRendererMixin.java
@@ -6,10 +6,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+@Mixin(net.minecraft.client.renderer.entity.WolfRenderer.class)
 public class WolfRendererMixin {
-    @Mixin(net.minecraft.client.renderer.entity.WolfRenderer.class)
-    public abstract class WolfRenderer {
         @Inject(method = "getTextureLocation", at = @At("HEAD"), cancellable = true)
         private void getTextureLocation(Wolf entity, CallbackInfoReturnable<ResourceLocation> cir) {
             if (entity.getPersistentData().contains("variant")) {
@@ -19,5 +17,4 @@ public class WolfRendererMixin {
                     cir.setReturnValue(customTexture);
             }
         }
-    }
 }
