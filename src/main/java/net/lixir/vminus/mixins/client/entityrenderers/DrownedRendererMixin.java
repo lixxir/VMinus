70570a2d586d4c536f1dcbe9779bef4c5f898dbf
lixxir <muffinsqw@outlook.com>
2024-11-23 19:35:37 -0400

    Fixed crash

--- src/main/java/net/lixir/vminus/mixins/client/entityrenderers/DrownedRendererMixin.java
+++ src/main/java/net/lixir/vminus/mixins/client/entityrenderers/DrownedRendererMixin.java
@@ -2,6 +2,7 @@ package net.lixir.vminus.mixins.client.entityrenderers;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.monster.Drowned;
+import net.minecraft.world.entity.monster.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(net.minecraft.client.renderer.entity.DrownedRenderer.class)
 public abstract class DrownedRendererMixin {
     @Inject(method = "getTextureLocation", at = @At("HEAD"), cancellable = true)
-    private void getTextureLocation(Drowned entity, CallbackInfoReturnable<ResourceLocation> cir) {
+    private void getTextureLocation(Zombie entity, CallbackInfoReturnable<ResourceLocation> cir) {
         if (entity.getPersistentData().contains("variant")) {
             String variant = entity.getPersistentData().getString("variant");
             ResourceLocation customTexture = new ResourceLocation("vminus:textures/entity/variants/drowned/" + variant + ".png");
--- src/main/java/net/lixir/vminus/mixins/client/entityrenderers/SkeletonRendererMixin.java
+++ src/main/java/net/lixir/vminus/mixins/client/entityrenderers/SkeletonRendererMixin.java
@@ -1,6 +1,7 @@
 package net.lixir.vminus.mixins.client.entityrenderers;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
 import net.minecraft.world.entity.monster.Skeleton;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(net.minecraft.client.renderer.entity.SkeletonRenderer.class)
 public abstract class SkeletonRendererMixin {
     @Inject(method = "getTextureLocation", at = @At("HEAD"), cancellable = true)
-    private void getTextureLocation(Skeleton entity, CallbackInfoReturnable<ResourceLocation> cir) {
+    private void getTextureLocation(AbstractSkeleton entity, CallbackInfoReturnable<ResourceLocation> cir) {
         if (entity.getPersistentData().contains("variant")) {
             String variant = entity.getPersistentData().getString("variant");
             ResourceLocation customTexture = new ResourceLocation("vminus:textures/entity/variants/skeleton/" + variant + ".png");
