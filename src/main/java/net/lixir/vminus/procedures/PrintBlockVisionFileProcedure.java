14e49426169c7ce24b154ee179226c83a4f723b9
lixxir <muffinsqw@outlook.com>
2024-11-26 20:44:25 -0400

    Added print key and cache statements to block vision raw command

--- src/main/java/net/lixir/vminus/procedures/PrintBlockVisionFileProcedure.java
+++ src/main/java/net/lixir/vminus/procedures/PrintBlockVisionFileProcedure.java
@@ -2,9 +2,12 @@ package net.lixir.vminus.procedures;
 
 import net.lixir.vminus.VMinusMod;
 import net.lixir.vminus.network.VminusModVariables;
+import net.lixir.vminus.visions.VisionHandler;
 
 public class PrintBlockVisionFileProcedure {
     public static void execute() {
         VMinusMod.LOGGER.debug(("Raw Block Vision File: " + VminusModVariables.main_block_vision));
+        VMinusMod.LOGGER.info(("Raw Block Vision Cache: " + VisionHandler.getBlockVisionCache()));
+        VMinusMod.LOGGER.info(("Raw Block Vision Key: " + VisionHandler.getBlockVisionKey()));
     }
 }
