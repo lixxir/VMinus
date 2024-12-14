a3ebae1febd129f61398faa3f985b1d42840aaca
lixxir <muffinsqw@outlook.com>
2024-12-14 14:42:36 -0400

    Fixed alexs mobs potion crash

--- src/main/java/net/lixir/vminus/BannedMobEffects.java
+++ src/main/java/net/lixir/vminus/BannedMobEffects.java
@@ -18,50 +18,16 @@ public class BannedMobEffects {
     @SubscribeEvent
     public static void onMobEffectEvent(MobEffectEvent.Applicable event) {
         if (event != null && event.getEntity() != null) {
-            String effect = event.getEffectInstance().toString();
-            int level = new Object() {
-                int convert(String s) {
-                    try {
-                        return (int) Double.parseDouble(s.trim());
-                    } catch (Exception e) {
+            MobEffect effect = event.getEffectInstance().getEffect();
+            if (effect == null)
+                return;
+            JsonObject visionData = VisionHandler.getVisionData(effect);
+            if (visionData != null && visionData.has("banned")) {
+                boolean banned = VisionValueHelper.isBooleanMet(visionData, "banned");
+                if (banned) {
+                    if (event != null && event.hasResult()) {
+                        event.setResult(Event.Result.DENY);
                     }
-                    return 0;
-                }
-            }.convert(effect.substring(effect.indexOf("x ") + "x ".length(), effect.indexOf(",")));
-            level = Math.max(1, level);
-            int duration = new Object() {
-                int convert(String s) {
-                    try {
-                        return (int) Double.parseDouble(s.trim());
-                    } catch (Exception e) {
-                    }
-                    return 0;
-                }
-            }.convert(effect.substring(effect.indexOf("Duration: ") + 10));
-            effect = effect.replace("effect.", "").replace(".", ":").replace(",", "");
-            effect = effect.substring(0, effect.indexOf(" "));
-            execute(event, effect);
-        }
-    }
-
-    public static void execute(String effect) {
-        execute(null, effect);
-    }
-
-    private static void execute(@Nullable Event event, String effect) {
-        if (effect == null)
-            return;
-        ResourceLocation effectLocation = new ResourceLocation(effect);
-        MobEffect mobEffect = ForgeRegistries.MOB_EFFECTS.getValue(effectLocation);
-        if (mobEffect == null) {
-            return;
-        }
-        JsonObject visionData = VisionHandler.getVisionData(mobEffect);
-        if (visionData != null && visionData.has("banned")) {
-            boolean banned = VisionValueHelper.isBooleanMet(visionData, "banned");
-            if (banned) {
-                if (event != null && event.hasResult()) {
-                    event.setResult(Event.Result.DENY);
                 }
             }
         }
