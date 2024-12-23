1b22696c807929631a4ca6301bb2fe9cbee6e498
lixxir <muffinsqw@outlook.com>
2024-12-22 21:26:30 -0400

    Fixed resource reloading from happening for each registered dimension

--- src/main/java/net/lixir/vminus/events/LevelLoadEventHandler.java
+++ src/main/java/net/lixir/vminus/events/LevelLoadEventHandler.java
@@ -1,5 +1,6 @@
 package net.lixir.vminus.events;
 
+import net.lixir.vminus.VMinusMod;
 import net.lixir.vminus.visions.ResourceVisionLoader;
 import net.lixir.vminus.visions.VisionHandler;
 import net.minecraft.server.MinecraftServer;
@@ -12,19 +13,27 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class LevelLoadEventHandler {
+    protected static boolean hasExecuted = false;
+
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void onWorldLoad(LevelEvent.Load event) {
+        if (hasExecuted) {
+            return;
+        }
+
         LevelAccessor world = event.getLevel();
         VisionHandler.clearCaches();
-        if (world instanceof ServerLevel _serverlevel) {
-            ResourceVisionLoader.generateItemVisionsFile(_serverlevel);
-            ResourceVisionLoader.generateBlockVisionsFile(_serverlevel);
-            ResourceVisionLoader.generateEntityVisionsFile(_serverlevel);
-            ResourceVisionLoader.generateEffectVisionsFile(_serverlevel);
-            ResourceVisionLoader.generateEnchantmentVisionsFile(_serverlevel);
-            // Required for vision recipe changes.
-            MinecraftServer server = _serverlevel.getServer();
+
+        if (world instanceof ServerLevel serverLevel) {
+            hasExecuted = true;
+            ResourceVisionLoader.generateItemVisionsFile(serverLevel);
+            ResourceVisionLoader.generateBlockVisionsFile(serverLevel);
+            ResourceVisionLoader.generateEntityVisionsFile(serverLevel);
+            ResourceVisionLoader.generateEffectVisionsFile(serverLevel);
+            ResourceVisionLoader.generateEnchantmentVisionsFile(serverLevel);
+            MinecraftServer server = serverLevel.getServer();
             if (server != null) {
+                VMinusMod.LOGGER.info("RELOADED RESOURCES.");
                 server.reloadResources(server.getPackRepository().getSelectedIds());
             }
         }
--- src/main/java/net/lixir/vminus/events/LevelUnloadEventHandler.java
+++ src/main/java/net/lixir/vminus/events/LevelUnloadEventHandler.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 public class LevelUnloadEventHandler {
     @SubscribeEvent
     public static void onWorldUnload(net.minecraftforge.event.level.LevelEvent.Unload event) {
+        LevelLoadEventHandler.hasExecuted = false;
         VminusModVariables.main_item_vision = new JsonObject();
         VminusModVariables.main_block_vision = new JsonObject();
         VminusModVariables.main_entity_vision = new JsonObject();
