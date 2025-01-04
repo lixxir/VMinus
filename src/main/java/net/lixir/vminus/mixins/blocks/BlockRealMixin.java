f4febac6f0e2b1beed0eb8b66118348496c12975
lixxir <112175761+lixxir@users.noreply.github.com>
2025-01-03 21:13:17 -0400

    Added key caching for individual block mixins

--- src/main/java/net/lixir/vminus/mixins/blocks/BlockRealMixin.java
+++ src/main/java/net/lixir/vminus/mixins/blocks/BlockRealMixin.java
@@ -2,8 +2,9 @@ package net.lixir.vminus.mixins.blocks;
 
 import com.google.gson.JsonObject;
 import net.lixir.vminus.SoundHelper;
+import net.lixir.vminus.VMinusMod;
 import net.lixir.vminus.visions.VisionHandler;
-import net.lixir.vminus.visions.VisionValueHandler;
+import net.lixir.vminus.visions.util.VisionValueHandler;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,52 +17,64 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Block.class)
 public abstract class BlockRealMixin {
     @Unique
-    private final Block block = (Block) (Object) this;
+    private final Block vminus$block = (Block) (Object) this;
 
-    @Inject(method = "getSpeedFactor", at = @At("HEAD"), cancellable = true)
+    @Unique
+    private int vminus$key = VisionHandler.EMPTY_KEY;
+
+    @Inject(method = "getSpeedFactor", at = @At("RETURN"), cancellable = true)
     private void getSpeedFactor(CallbackInfoReturnable<Float> cir) {
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(vminus$block);
+        JsonObject blockData = VisionHandler.getVisionData(vminus$block, vminus$key);
         if (blockData != null && blockData.has("speed_factor")) {
-            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "speed_factor", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, block));
+            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "speed_factor", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, vminus$block));
         }
     }
 
-    @Inject(method = "getFriction", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getFriction", at = @At("RETURN"), cancellable = true)
     private void getFriction(CallbackInfoReturnable<Float> cir) {
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(vminus$block);
+        JsonObject blockData = VisionHandler.getVisionData(vminus$block, vminus$key);
         if (blockData != null && blockData.has("friction")) {
-            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "friction", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, block));
+            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "friction", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, vminus$block));
         }
     }
 
-    @Inject(method = "getJumpFactor", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getJumpFactor", at = @At("RETURN"), cancellable = true)
     private void getJumpFactor(CallbackInfoReturnable<Float> cir) {
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(vminus$block);
+        JsonObject blockData = VisionHandler.getVisionData(vminus$block, vminus$key);
         if (blockData != null && blockData.has("jump_factor")) {
-            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "jump_factor", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, block));
+            cir.setReturnValue(VisionValueHandler.isNumberMet(blockData, "jump_factor", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, vminus$block));
         }
     }
 
-    @Inject(method = "getExplosionResistance", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getExplosionResistance", at = @At("RETURN"), cancellable = true)
     private void getExplosionResistance(CallbackInfoReturnable<Float> cir) {
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(vminus$block);
+        JsonObject blockData = VisionHandler.getVisionData(vminus$block, vminus$key);
         if (blockData != null && blockData.has("explosion_resistance")) {
-            cir.setReturnValue(Math.max(VisionValueHandler.isNumberMet(blockData, "explosion_resistance", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, block), -1));
+            cir.setReturnValue(Math.max(VisionValueHandler.isNumberMet(blockData, "explosion_resistance", cir.getReturnValue() != null ? cir.getReturnValue() : 1.0F, vminus$block), -1));
         }
     }
 
-    @Inject(method = "getSoundType", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getSoundType", at = @At("RETURN"), cancellable = true)
     private void getSoundType(BlockState state, CallbackInfoReturnable<SoundType> cir) {
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(vminus$block);
+        JsonObject blockData = VisionHandler.getVisionData(vminus$block, vminus$key);
         if (blockData != null && blockData.has("sound")) {
-            String breakSound = VisionValueHandler.getFirstValidString(blockData, "sound", block, "break");
-            String stepSound = VisionValueHandler.getFirstValidString(blockData, "sound", block, "step");
-            String placeSound = VisionValueHandler.getFirstValidString(blockData, "sound", block, "place");
-            String hitSound = VisionValueHandler.getFirstValidString(blockData, "sound", block, "hit");
-            String fallSound = VisionValueHandler.getFirstValidString(blockData, "sound", block, "fall");
+            String breakSound = VisionValueHandler.getFirstValidString(blockData, "sound", vminus$block, "break");
+            String stepSound = VisionValueHandler.getFirstValidString(blockData, "sound", vminus$block, "step");
+            String placeSound = VisionValueHandler.getFirstValidString(blockData, "sound", vminus$block, "place");
+            String hitSound = VisionValueHandler.getFirstValidString(blockData, "sound", vminus$block, "hit");
+            String fallSound = VisionValueHandler.getFirstValidString(blockData, "sound", vminus$block, "fall");
             SoundType soundType = SoundHelper.CreateBlockSoundType(breakSound, stepSound, placeSound, hitSound, fallSound);
-            if (soundType != null)
-                cir.setReturnValue(soundType);
+            cir.setReturnValue(soundType);
         }
     }
 }
--- src/main/java/net/lixir/vminus/mixins/blocks/BlockStateBaseMixin.java
+++ src/main/java/net/lixir/vminus/mixins/blocks/BlockStateBaseMixin.java
@@ -2,7 +2,7 @@ package net.lixir.vminus.mixins.blocks;
 
 import com.google.gson.JsonObject;
 import net.lixir.vminus.visions.VisionHandler;
-import net.lixir.vminus.visions.VisionValueHandler;
+import net.lixir.vminus.visions.util.VisionValueHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
@@ -19,12 +19,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BlockBehaviour.BlockStateBase.class)
 public abstract class BlockStateBaseMixin {
     @Unique
-    private final BlockState state = (BlockState) (Object) this;
+    private final BlockState vminus$state = (BlockState) (Object) this;
+    @Unique
+    private int vminus$key = VisionHandler.EMPTY_KEY;
 
     @Inject(method = "getLightEmission", at = @At("HEAD"), cancellable = true)
     private void getLightEmission(CallbackInfoReturnable<Integer> cir) {
-        Block block = state.getBlock();
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        Block block = vminus$state.getBlock();
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(block);
+        JsonObject blockData = VisionHandler.getVisionData(block,vminus$key);
         if (blockData != null && blockData.has("light_level")) {
             int lightLevel = VisionValueHandler.isNumberMet(blockData, "light_level", (cir.getReturnValue() != null ? cir.getReturnValue() : 0), block);
             cir.setReturnValue(Math.min(Math.max(lightLevel, 0), 15));
@@ -33,8 +37,10 @@ public abstract class BlockStateBaseMixin {
 
     @Inject(method = "emissiveRendering", at = @At("HEAD"), cancellable = true)
     private void emissiveRendering(CallbackInfoReturnable<Boolean> cir) {
-        Block block = state.getBlock();
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        Block block = vminus$state.getBlock();
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(block);
+        JsonObject blockData = VisionHandler.getVisionData(block,vminus$key);
         if (blockData != null && blockData.has("emissive_rendering")) {
             cir.setReturnValue(VisionValueHandler.isBooleanMet(blockData, "emissive_rendering", block));
         }
@@ -42,8 +48,10 @@ public abstract class BlockStateBaseMixin {
 
     @Inject(method = "canOcclude", at = @At("HEAD"), cancellable = true)
     private void canOcclude(CallbackInfoReturnable<Boolean> cir) {
-        Block block = state.getBlock();
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        Block block = vminus$state.getBlock();
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(block);
+        JsonObject blockData = VisionHandler.getVisionData(block,vminus$key);
         if (blockData != null && blockData.has("can_occlude")) {
             cir.setReturnValue(VisionValueHandler.isBooleanMet(blockData, "can_occlude", block));
         }
@@ -51,8 +59,10 @@ public abstract class BlockStateBaseMixin {
 
     @Inject(method = "isRedstoneConductor", at = @At("HEAD"), cancellable = true)
     private void isRedstoneConductor(CallbackInfoReturnable<Boolean> cir) {
-        Block block = state.getBlock();
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        Block block = vminus$state.getBlock();
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(block);
+        JsonObject blockData = VisionHandler.getVisionData(block,vminus$key);
         if (blockData != null && blockData.has("redstone_conductor")) {
             cir.setReturnValue(VisionValueHandler.isBooleanMet(blockData, "redstone_conductor", block));
         }
@@ -60,8 +70,10 @@ public abstract class BlockStateBaseMixin {
 
     @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
     private void getDestroySpeed(BlockGetter p_60801_, BlockPos p_60802_, CallbackInfoReturnable<Float> cir) {
-        Block block = state.getBlock();
-        JsonObject blockData = VisionHandler.getVisionData(block);
+        Block block = vminus$state.getBlock();
+        if (vminus$key == -1)
+            vminus$key = VisionHandler.getCacheKey(block);
+        JsonObject blockData = VisionHandler.getVisionData(block,vminus$key);
         if (blockData != null && blockData.has("destroy_time")) {
             float destroyTime = VisionValueHandler.isNumberMet(blockData, "destroy_time", (cir.getReturnValue() != null ? cir.getReturnValue() : 0), block);
             cir.setReturnValue(Math.max(-1, destroyTime));
@@ -70,8 +82,7 @@ public abstract class BlockStateBaseMixin {
 
     @Inject(method = "isValidSpawn", at = @At("HEAD"), cancellable = true)
     private void isValidSpawn(CallbackInfoReturnable<Boolean> cir) {
-        BlockState state = (BlockState) (Object) this;
-        if (state.is(BlockTags.create(new ResourceLocation("vminus:override_valid_spawning")))) {
+        if (vminus$state.is(BlockTags.create(new ResourceLocation("vminus:override_valid_spawning")))) {
             cir.setReturnValue(true);
         }
     }
