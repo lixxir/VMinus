bb747c7e32e89e3eccab2ee1fec2518ec9171851
lixxir <112175761+lixxir@users.noreply.github.com>
2025-01-03 21:17:09 -0400

    Removed resource reload and added a debounce check for clearing and generating caches

--- src/main/java/net/lixir/vminus/events/LevelLoadEventHandler.java
+++ src/main/java/net/lixir/vminus/events/LevelLoadEventHandler.java
@@ -1,22 +1,29 @@
 package net.lixir.vminus.events;
 
+import com.google.gson.JsonElement;
 import net.lixir.vminus.VMinusMod;
 import net.lixir.vminus.visions.ResourceVisionLoader;
 import net.lixir.vminus.visions.VisionHandler;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Map;
+
 @Mod.EventBusSubscriber
 public class LevelLoadEventHandler {
-    protected static boolean hasExecuted = false;
+    protected static volatile boolean hasExecuted = false;
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
-    public static void onWorldLoad(LevelEvent.Load event) {
+    synchronized public static void onWorldLoad(LevelEvent.Load event) {
         if (hasExecuted) {
             return;
         }
@@ -31,10 +38,16 @@ public class LevelLoadEventHandler {
             ResourceVisionLoader.generateEntityVisionsFile(serverLevel);
             ResourceVisionLoader.generateEffectVisionsFile(serverLevel);
             ResourceVisionLoader.generateEnchantmentVisionsFile(serverLevel);
+            /*
             MinecraftServer server = serverLevel.getServer();
             if (server != null) {
-                server.reloadResources(server.getPackRepository().getSelectedIds());
+                server.reloadResources(server.getPackRepository().getSelectedIds()).thenRun(() -> VMinusMod.LOGGER.info("Resource reload completed.")).exceptionally(throwable -> {
+                    VMinusMod.LOGGER.error("Resource reload failed", throwable);
+                    return null;
+                });
             }
+
+             */
         }
         VisionHandler.loadVisions();
     }
