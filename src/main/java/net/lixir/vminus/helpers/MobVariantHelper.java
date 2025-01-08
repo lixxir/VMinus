67695db0f4372cb9fd71b1ef8e72b9ab20525c33
lixxir <112175761+lixxir@users.noreply.github.com>
2025-01-07 21:35:13 -0400

    Uses java random instead of random source as that does not exist

--- src/main/java/net/lixir/vminus/helpers/MobVariantHelper.java
+++ src/main/java/net/lixir/vminus/helpers/MobVariantHelper.java
@@ -3,11 +3,11 @@ package net.lixir.vminus.helpers;
 import com.google.gson.JsonObject;
 import net.lixir.vminus.visions.util.VisionValueHandler;
 import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MobVariantHelper {
     public static String setOrGetVariant(Entity entity, JsonObject visionData) {
@@ -15,7 +15,7 @@ public class MobVariantHelper {
         if (chosenVariant.isEmpty()) {
             List<String> variants = VisionValueHandler.getListOfStrings(visionData, "variants", entity);
             chosenVariant = !variants.isEmpty()
-                    ? variants.get(Mth.nextInt(RandomSource.create(), 0, variants.size() - 1))
+                    ? variants.get(Mth.nextInt(ThreadLocalRandom.current(), 0, variants.size() - 1))
                     : "normal";
             entity.getPersistentData().putString("variant", chosenVariant);
         }
@@ -23,14 +23,12 @@ public class MobVariantHelper {
     }
 
     public static void setVariant(Entity entity, @Nullable String variant) {
-        if (variant != null || !variant.isEmpty()) {
+        if (variant != null && !variant.isEmpty()) {
             entity.getPersistentData().putString("variant", variant);
         }
     }
 
     public static String getVariant(Entity entity) {
-        String chosenVariant = entity.getPersistentData().getString("variant");
-        return chosenVariant;
+        return entity.getPersistentData().getString("variant");
     }
-
 }
